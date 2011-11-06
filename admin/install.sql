ea70947141018e3fbf804504e4eda729150b56f2
Robert Gerald Porter <rob@cartanova.ca>
2011-11-06 18:18:42 -0500

    References to '__weever_tabs' table removed

--- admin/install.sql
+++ admin/install.sql
@@ -1,24 +1,3 @@
-
-CREATE TABLE IF NOT EXISTS `#__weever_tabs` (
-  `name` varchar(255) NOT NULL,
-  `component` varchar(255) NOT NULL,
-  `published` tinyint(1) NOT NULL default '1',
-  `component_id` int(11) NOT NULL,
-  `id` int(11) NOT NULL auto_increment,
-  `icon` varchar(255) NOT NULL default 'icon.png',
-  `component_behaviour` varchar(255) NOT NULL,
-  `parent_tab_id` int(11) NOT NULL default '0',
-  `cloud_tab_id` int(11) NOT NULL,
-  `default` tinyint(1) NOT NULL default '0',
-  `cms_feed` tinytext NOT NULL,
-  `var` tinytext NOT NULL,
-  `hash` varchar(64) NOT NULL,
-  `ordering` decimal(11,1) NOT NULL default '1.0',
-  `type` varchar(63) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
-
-
 CREATE TABLE IF NOT EXISTS `#__weever_config` (
   `id` int(11) NOT NULL auto_increment,
   `option` varchar(55) NOT NULL,
