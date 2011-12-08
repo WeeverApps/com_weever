a4f6aa12bd685017d4a6d5b8c92a05232ae1d98d
Robert Gerald Porter <rob@cartanova.ca>
2011-12-08 00:44:07 -0500

    [Fixes #21963425]

--- admin/classes/fileuploader.php
+++ admin/classes/fileuploader.php
@@ -5,7 +5,7 @@
 *	(c) 2010-2011 Weever Apps Inc. <http://www.weeverapps.com/>
 *
 *	Author: 	Robert Gerald Porter <rob@weeverapps.com>
-*	Version: 	1.3.0.1
+*	Version: 	1.3.1
 *   License: 	GPL v3.0
 *
 *   This extension is free software: you can redistribute it and/or modify
